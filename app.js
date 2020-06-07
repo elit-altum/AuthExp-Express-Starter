cf2af0ccab3b335d96eaab7097f22cc75ac5a3a2
elit-altum <manan.sharma311@gmail.com>
2020-06-07 20:19:03 +0530

    add: security and sanitising middlewares

--- app.js
+++ app.js
@@ -2,19 +2,24 @@ require("dotenv").config({
 	path: "config.env",
 });
 
-// Middlewares, Routers and Mongo connection
 const express = require("express");
 const mongoose = require("mongoose");
+
+// 1. Middlewares
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const xss = require("xss-clean");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const compression = require("compression");
 
-// Importing routers
+// 2. Importing routers
 const userRouter = require("./routes/userRouter");
 const errorHandler = require("./utils/errorHandler");
 
 const app = express();
 
-// Connect to MongoDB
+// 3. Connect to MongoDB
 mongoose
 	.connect(process.env.MONGO_SRV, {
 		useNewUrlParser: true,
@@ -29,10 +34,15 @@ mongoose
 		console.log("Cannot connect to database:", err);
 	});
 
+// *? 4. ATTACH MIDDLEWARES
+
 // Morgan for request logging
 if (process.env.NODE_ENV === "development") {
 	app.use(morgan("dev"));
 }
+
+// Proxies with Heroku, ngnix etc
+app.set("trust proxy", 1);
 
 // Parse request body
 app.use(express.json());
@@ -40,10 +50,22 @@ app.use(express.json());
 // Parse cookies
 app.use(cookieParser());
 
-// Mounting app routers
+// Sanitize xss input
+app.use(xss());
+
+// Sanitize mongo NoSQL queries
+app.use(mongoSanitize);
+
+// General security headers
+app.use(helmet());
+
+// Compress request and response
+app.use(compression());
+
+// *? 5. MOUNT API ROUTERS
 app.use("/api/v1/users", userRouter);
 
-// Error handler
+// *? 6. ERROR HANDLERS
 app.use(errorHandler);
 
 module.exports = app;
